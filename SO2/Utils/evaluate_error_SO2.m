97cc6a564871432bcc6f13da150c35cda799567b
Yunpeng Shi <yunpengs@princeton.edu>
2021-12-16 21:33:38 -0500

    Update evaluate_error_SO2.m

--- SO2/Utils/evaluate_error_SO2.m
+++ SO2/Utils/evaluate_error_SO2.m
@@ -2,7 +2,7 @@ function [mean_err] = evaluate_error_SO2(theta_est, thetaij_orig, Ind)
         Ind_i = Ind(:,1);
         Ind_j = Ind(:,2);
         m = length(Ind_i);
-        zij_est = zeros(1,m);
+        thetaij_est = zeros(1,m);
         for k = 1:m
             i=Ind_i(k); j=Ind_j(k); 
             thetaij_est(k)=theta_est(i)-theta_est(j);
