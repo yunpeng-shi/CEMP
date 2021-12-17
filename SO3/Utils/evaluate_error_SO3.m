4e85b714138347fa738911090c30c42f9987a7b7
Yunpeng Shi <yunpengs@princeton.edu>
2021-12-17 15:15:35 -0500

    Add files via upload

--- SO3/Utils/evaluate_error_SO3.m
+++ SO3/Utils/evaluate_error_SO3.m
@@ -1,1 +1,19 @@
+function [mean_err] = evaluate_error_SO3(R_est, Rij_orig, Ind)
+        Ind_i = Ind(:,1);
+        Ind_j = Ind(:,2);
+        m = length(Ind_i);
+        d = 3;
+        Rij_est = zeros(d,d,m);
+        for k = 1:m
+            i=Ind_i(k); j=Ind_j(k); 
+            Rij_est(:,:,k)=R_est(:,:,i)*(R_est(:,:,j)');
+        end
+        
+        MSEVec = zeros(1,m);
+        for k = 1:m
+            R_tr = trace(Rij_orig(:,:,k)*(Rij_est(:,:,k))');
+            MSEVec(k) =  abs(acos((R_tr-1)./2))/pi;
+        end
+        mean_err = mean(MSEVec);
 
+end
