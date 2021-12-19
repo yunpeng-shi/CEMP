d20ab06cc3077c0b46bf953d70903d64bfa5a47f
Yunpeng Shi <yunpengs@princeton.edu>
2021-12-18 22:11:10 -0500

    Update CEMP_MST_SOd.m

--- SOd/Algorithms/CEMP_MST_SOd.m
+++ SOd/Algorithms/CEMP_MST_SOd.m
@@ -53,7 +53,6 @@ function R_est = CEMP_MST_SOd(Ind,RijMat,parameters)
     Ind_ki = zeros(1,m_cycle);
     
     RijMat4d = zeros(d,d,n,n);
-    % store pairwise directions in 3 by n by n tensor
     % construct edge index matrix (for 2d-to-1d index conversion)
     for l = 1:m
         i=Ind_i(l);j=Ind_j(l);
@@ -62,13 +61,6 @@ function R_est = CEMP_MST_SOd(Ind,RijMat,parameters)
         IndMat(i,j)=l;
         IndMat(j,i)=-l;
     end
-    
-    
-    
-    % CoIndMat{l}= triangles sampled that contains l-th edge
-    % e.g. l=(3,5), then CoIndMat{l}=[2,9,8,...] means that...
-    % triangles 352, 359, 358,... are sampled
-    
     
     
     
@@ -140,12 +132,10 @@ function R_est = CEMP_MST_SOd(Ind,RijMat,parameters)
     while beta <= beta_max
          
         
-        % parameter controling the decay rate of reweighting function
-        
         Sjk = SVec(Ind_jk);
         Ski = SVec(Ind_ki);
         S_sum = Ski+Sjk;
-        % compute weight matrix (nsample by m)
+       
         Weight_vec = exp(-beta*S_sum);
         S0_weight = S0_long.*Weight_vec;
     
